8c4230e0b6b779de0be3398aee74a79db6ba89ed
Andrew Muscara <andrewmuscara@gmail.com>
2018-10-22 13:07:57 -0700

    andrew

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ var config = {
     authDomain: "group-project1-35b77.firebaseapp.com",
     databaseURL: "https://group-project1-35b77.firebaseio.com",
     projectId: "group-project1-35b77",
-<<<<<<< HEAD
     storageBucket: "",
-=======
-    storageBucket: "group-project1-35b77.appspot.com",
->>>>>>> 186045fd1be7fc4a7db439b4f60b5a5ea5cf854e
     messagingSenderId: "433243594797"
 };
 firebase.initializeApp(config);
@@ -49,7 +45,6 @@ $(".regiserButton").on("click", function handleSignUp(event) {
        
     });
 
-<<<<<<< HEAD
 var confirmPassword = ""
 function clearCreateAccount() {
  
@@ -83,76 +78,5 @@ $("#registerButton").on("click", function (event) {
 // function(errorObject) {
 //     console.log("The read failed: " + errorObject.code);
 //   });
-=======
-})
-
-
-$(".loginButton").on("click", function toggleSignIn(event) {
-    event.preventDefault()
-    if (firebase.auth().currentUser) {
-        // [START signout]
-        firebase.auth().signOut();
-        // [END signout]
-    } else {
-        var email = $("#userLogin").val().trim();
-        var password = $("#passwordLogin").val().trim();
-        if (email.length < 4) {
-            $("#errorLoginUser").html("<h3> Please enter an email address </h3>");
-            return;
-        }
-        if (password.length < 4) {
-            $("#errorLoginPassword").html("<h3> Please create a password with more than 4 letters </h3>");
-            return;
-        }
-        // Sign in with email and pass.
-
-        firebase.auth().signInWithEmailAndPassword(email, password).then(function () {
-            window.location = 'index.html'
-        }).catch(function (error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // [START_EXCLUDE]
-            if (errorCode === 'auth/wrong-password') {
-                $("#errorLoginPassword").html("<h3> Wrong Password! Please try again. </h3>");
-               
-            } else {
-                $("#errorLoginUser").html(errorMessage);
-            }
-            console.log(error);
-
-
-        });
-
-    }
-
-})
-
-
-
-$(".signoutButton").on("click", function handleSignUp() {
-   
-    auth.signOut().then(function() {
-        window.location = 'loginpage.html'
-        console.log('Signed Out');
-      }, function(error) {
-        console.error('Sign Out Error', error);
-      });
-
-
-})
-
-
-auth.onAuthStateChanged(function (user) {
-
-    if (user) {
-        console.log("working")
-        // window.location = 'index.html'
-    }
-
-    else {
-        console.log("not working")
-    }
->>>>>>> 186045fd1be7fc4a7db439b4f60b5a5ea5cf854e
 
 })
